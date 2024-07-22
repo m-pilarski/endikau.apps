6c946a20d3816d3fa345bd5b9543d6e21873f02d
m-pilarski <pilarski@cryptolab.net>
2024-07-22 12:37:57 +0200

    test

---
+++ R/run_sentiment_app.R
@@ -0,0 +1,11 @@
+#' run_app_sentiment
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' 1 + 1
+run_app_sentiment <- function() {
+  app_dir <- system.file("app", "sentiment", package="endikau.apps")
+  shiny::runApp(app_dir, display.mode="normal")
+}

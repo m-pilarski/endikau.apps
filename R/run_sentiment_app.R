16e9b0dd5096812f243b750f4c6138bbc75888b6
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2024-08-07 15:53:22 +0200

    update

--- R/run_sentiment_app.R
+++ R/run_sentiment_app.R
@@ -1,12 +1,12 @@
-#' run_app_sentiment
+#' run_app_sentiment_dict
 #'
 #' @return NULL
 #' @export
 #'
 #' @examples
 #' 1 + 1
-run_app_sentiment <- function() {
-  app_dir <- system.file("app", "sentiment", package="endikau.apps")
+run_app_sentiment_dict <- function() {
+  app_dir <- system.file("app", "sentiment_dict", package="endikau.apps")
   # shiny::runApp(app_dir, display.mode="normal")
   shiny::shinyAppDir(appDir=app_dir)
 }

3396fae3996de8fa5ac6b03ffce6035ba48d901c
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2024-08-08 08:49:38 +0200

    update

--- inst/app/sentiment_dict/global.R
+++ inst/app/sentiment_dict/global.R
@@ -3,27 +3,23 @@ library(stringi)
 
 `%||%` <- rlang::`%||%`
 
-sentiws_tbl <- fst::read_fst(here::here("data/sentiws_tbl.fst"))
-gerpolclu_tbl <- fst::read_fst(here::here("data/gerpolclu_tbl.fst"))
-amazon_review_tbl <- fst::read_fst(here::here("data/amazon_review_tbl.fst"))
-
 calc_doc_sentiment_tbl <- function(.doc_vec, .sentiment_tbl){
   .doc_vec %||%
-    character() |> 
-    tibble::as_tibble_col("doc_text_str") |> 
-    tibble::rowid_to_column(var="doc_id") |> 
+    character() |>
+    tibble::as_tibble_col("doc_text_str") |>
+    tibble::rowid_to_column(var="doc_id") |>
     dplyr::mutate(
-      doc_tok_str_lst = doc_text_str |> 
-        stringi::stri_split_boundaries(type="word") |> 
+      doc_tok_str_lst = doc_text_str |>
+        stringi::stri_split_boundaries(type="word") |>
         purrr::map(stringi::stri_subset_charclass, "[[:alnum:]]"),
       .keep="unused"
-    ) |> 
-    tidyr::unnest_longer(doc_tok_str_lst, values_to="tok_str_raw") |> 
-    dplyr::mutate(tok_str_prep = stringi::stri_trans_tolower(tok_str_raw)) |> 
+    ) |>
+    tidyr::unnest_longer(doc_tok_str_lst, values_to="tok_str_raw") |>
+    dplyr::mutate(tok_str_prep = stringi::stri_trans_tolower(tok_str_raw)) |>
     dplyr::left_join(
-      .sentiment_tbl, by=dplyr::join_by(tok_str_prep == tok_str), 
+      .sentiment_tbl, by=dplyr::join_by(tok_str_prep == tok_str),
       relationship="many-to-one", multiple="any"
-    ) |> 
+    ) |>
     dplyr::mutate(
       tok_pol_lab = tidyr::replace_na(
         tok_pol_lab, factor("sen-neu", levels=levels(tok_pol_lab))
--- inst/app/sentiment_dict/server.R
+++ inst/app/sentiment_dict/server.R
@@ -5,29 +5,29 @@ function(input, output, session) {
   vals <- reactiveValues(
     sen_vec = character(),
     # sen_vec = c("ich mag das nicht", "das gefällt mir"),
-    senti_dict_tbl = sentiws_tbl
+    senti_dict_tbl = endikau.data::sentiws_tbl
   )
 
   observeEvent(
     input$senti_dict, {
       if(input$senti_dict == "SentiWS"){
-        vals$senti_dict_tbl <- sentiws_tbl
+        vals$senti_dict_tbl <- endikau.data::sentiws_tbl
       }
       if(input$senti_dict == "German Polarity Clues"){
-        vals$senti_dict_tbl <- gerpolclu_tbl
+        vals$senti_dict_tbl <- endikau.data::gerpolclu_tbl
       }
     }
   )
-  
+
   output$accordion <- renderUI({
-    
-    .accordion_tbl <- 
+
+    .accordion_tbl <-
       .doc_sentiment_tbl_rct() |>
       dplyr::summarize(
         doc_text_html = stringi::stri_c(
           stringi::stri_c(
             "<span class='", tok_pol_lab, " bubble' data-toggle='tooltip' ",
-            "data-placement='top' title='", tok_pol_num, "'>", 
+            "data-placement='top' title='", tok_pol_num, "'>",
             tok_str_raw, "</span>"
           ),
           collapse=""
@@ -35,19 +35,19 @@ function(input, output, session) {
         doc_word_count = length(tok_str_raw),
         doc_pol_norm = sum(tok_pol_num) / doc_word_count,
         .by=doc_id
-      ) |> 
+      ) |>
       mutate(
         doc_panel = list(
           accordion_panel(
             title=HTML(stringi::stri_c(
               "<table style='width:100%'><tr>",
               "  <td width=80%>", doc_text_html, "</td>",
-              "  <td>", 
+              "  <td>",
               scales::label_number(
                 accuracy=1e-3, drop0trailing=FALSE
-              )(doc_pol_norm), 
+              )(doc_pol_norm),
               "  </td>",
-              "  <td>", 
+              "  <td>",
               as.character(shiny::actionButton(
                 inputId = paste0("delete_row", doc_id),
                 label=NULL,
@@ -58,35 +58,35 @@ function(input, output, session) {
                   "  'delete_row', this.id, {priority: 'event'}",
                   ")"
                 )
-              )), 
+              )),
               "  </td>",
               "<tr></table>"
             )),
             HTML(as.character(
               withMathJax("$$a^2 + b^2 = c^2$$")
-            #   # stringi::stri_rand_lipsum(n_paragraphs=1), 
+            #   # stringi::stri_rand_lipsum(n_paragraphs=1),
             #   # style="font-size:1rem; --bs-body-font-size:1rem;"
             ))
           )
         ),
         .by=doc_id
       )
-    
+
     HTML(as.character(accordion(!!!.accordion_tbl$doc_panel, multiple=FALSE)))
-    
+
   })
-  
+
   observeEvent(
     input$sen_random, {
       updateTextInput(
-        session=session, inputId="sen_input", 
-        value=with(dplyr::slice_sample(amazon_review_tbl, n=1), {
+        session=session, inputId="sen_input",
+        value=with(dplyr::slice_sample(endikau.data::amazon_review_tbl, n=1), {
           stringi::stri_c(doc_title, ": ", doc_text)
         })
       )
     }
   )
-  
+
   observeEvent(
     input$sen_add, {
       vals$sen_vec <- c(input$sen_input, vals$sen_vec)
@@ -97,7 +97,7 @@ function(input, output, session) {
   .doc_sentiment_tbl_rct <- reactive(
     calc_doc_sentiment_tbl(vals$sen_vec, .sentiment_tbl=vals$senti_dict_tbl)
   )
-  
+
   observeEvent(input$delete_row, {
     idx <- as.integer(
       stringi::stri_extract_first_regex(
@@ -106,21 +106,21 @@ function(input, output, session) {
     )
     vals$sen_vec <- vals$sen_vec[-idx]
   })
-  
+
   output$sentiment_table <- gt::render_gt({
-    
+
     .table_data <- .doc_sentiment_tbl_rct()
-    
+
     if(nrow(.table_data) == 0){
       NULL
     }else{
-      
+
     .doc_sentiment_tbl_rct() |>
       dplyr::summarize(
         doc_text_html = stringi::stri_c(
           stringi::stri_c(
             "<span class='", tok_pol_lab, " bubble' data-toggle='tooltip' ",
-            "data-placement='top' title='", tok_pol_num, "'>", 
+            "data-placement='top' title='", tok_pol_num, "'>",
             tok_str_raw, "</span>"
           ),
           collapse=""
@@ -164,25 +164,25 @@ function(input, output, session) {
       )
     }
   })
-  
+
   output$senti_dict_tbl <- gt::render_gt({
-    .senti_dict_smpl_tbl <<- 
-      vals$senti_dict_tbl |> 
-      dplyr::slice_sample(n=5) |> 
+    .senti_dict_smpl_tbl <<-
+      vals$senti_dict_tbl |>
+      dplyr::slice_sample(n=5) |>
       dplyr::arrange(tok_pol_num)
-    
-    .senti_dict_smpl_gt <<- 
-      .senti_dict_smpl_tbl |> 
-      dplyr::slice_sample(n=5) |> 
-      dplyr::arrange(tok_pol_num) |> 
+
+    .senti_dict_smpl_gt <<-
+      .senti_dict_smpl_tbl |>
+      dplyr::slice_sample(n=5) |>
+      dplyr::arrange(tok_pol_num) |>
       gt::gt()
-    
-    .senti_dict_smpl_tbl |> 
-      tibble::rowid_to_column() |> 
-      dplyr::group_by(tok_pol_lab) |> 
+
+    .senti_dict_smpl_tbl |>
+      tibble::rowid_to_column() |>
+      dplyr::group_by(tok_pol_lab) |>
       dplyr::group_walk(\(..gdata, ..gkeys){
-        .senti_dict_smpl_gt <<- 
-          .senti_dict_smpl_gt |> 
+        .senti_dict_smpl_gt <<-
+          .senti_dict_smpl_gt |>
           gt::fmt(
             columns=tok_str,
             rows=..gdata$rowid,
@@ -194,9 +194,9 @@ function(input, output, session) {
             })}
           )
       })
-    
+
     .senti_dict_smpl_gt
-    
+
   })
-  
+
 }

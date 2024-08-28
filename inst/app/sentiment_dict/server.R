2ce9fb782960dac3e8729eb633af0eae03334c7f
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2024-08-28 11:28:03 +0200

    update

--- inst/app/sentiment_dict/server.R
+++ inst/app/sentiment_dict/server.R
@@ -1,8 +1,9 @@
 library(shiny)
-library(future)
-library(promises)
-
-plan(multisession)
+library(callr)
+# library(future)
+# library(promises)
+#
+# plan(multisession)
 
 example_review <- paste0(
   "Leider nicht erhalten. Schade, dass der Artikel bis heute noch nicht ",
@@ -16,11 +17,11 @@ function(input, output, session) {
 
   vals <- reactiveValues(
     sen_vec = example_review,
-    # sen_vec = c("ich mag das nicht", "das gefällt mir"),
     sentidict_tbl = endikau.data::sentiws_tbl,
   )
 
   .doc_germansentiment_tbl_rct <- reactiveVal(NULL)
+  .germansentiment_calculating_rct <- reactiveVal(FALSE)
 
   observeEvent(
     input$sentidict, {
@@ -32,63 +33,6 @@ function(input, output, session) {
       }
     }
   )
-
-  # output$accordion <- renderUI({
-  #
-  #   .accordion_tbl <-
-  #     .doc_sentidict_tbl_rct() |>
-  #     dplyr::summarize(
-  #       doc_text_html = stringi::stri_c(
-  #         stringi::stri_c(
-  #           "<span class='", tok_pol_lab, " bubble' data-toggle='tooltip' ",
-  #           "data-placement='top' title='", tok_pol_num, "'>",
-  #           tok_str_raw, "</span>"
-  #         ),
-  #         collapse=""
-  #       ),
-  #       doc_word_count = length(tok_str_raw),
-  #       doc_pol_norm = sum(tok_pol_num) / doc_word_count,
-  #       .by=doc_id
-  #     ) |>
-  #     mutate(
-  #       doc_panel = list(
-  #         accordion_panel(
-  #           title=HTML(stringi::stri_c(
-  #             "<table style='width:100%'><tr>",
-  #             "  <td width=80%>", doc_text_html, "</td>",
-  #             "  <td>",
-  #             scales::label_number(
-  #               accuracy=1e-3, drop0trailing=FALSE
-  #             )(doc_pol_norm),
-  #             "  </td>",
-  #             "  <td>",
-  #             as.character(shiny::actionButton(
-  #               inputId = paste0("delete_row", doc_id),
-  #               label=NULL,
-  #               icon=icon("trash-can"),
-  #               style="padding: 0; border: none; background: none;",
-  #               onclick=stringi::stri_c(
-  #                 "Shiny.setInputValue(",
-  #                 "  'delete_row', this.id, {priority: 'event'}",
-  #                 ")"
-  #               )
-  #             )),
-  #             "  </td>",
-  #             "<tr></table>"
-  #           )),
-  #           HTML(as.character(
-  #             withMathJax("$$a^2 + b^2 = c^2$$")
-  #           #   # stringi::stri_rand_lipsum(n_paragraphs=1),
-  #           #   # style="font-size:1rem; --bs-body-font-size:1rem;"
-  #           ))
-  #         )
-  #       ),
-  #       .by=doc_id
-  #     )
-  #
-  #   HTML(as.character(accordion(!!!.accordion_tbl$doc_panel, multiple=FALSE)))
-  #
-  # })
 
   observeEvent(
     input$`sen_random-1`, {
@@ -102,7 +46,6 @@ function(input, output, session) {
   observeEvent(
     input$`sen_add-1`, {
       vals$sen_vec <- c(input$`sen_input-1`, vals$sen_vec)
-      updateTextInput(session=session, inputId="sen_input-1", value="")
     }
   )
 
@@ -116,99 +59,29 @@ function(input, output, session) {
     )
   )
 
-  .doc_germansentiment_tbl_task <- ExtendedTask$new(function(.doc_str) {
-    future_promise({
-      vns::use_vns_condaenv()
-      vns::calc_doc_germansentiment_tbl(.doc_str=.doc_str)
-    })
-  })
-
+  .germansentiment_rsess <- r_session$new(wait=TRUE)
   observe({
-    .doc_germansentiment_tbl_task$invoke(.doc_str=first(vals$sen_vec))
-  })
-
-
-  observeEvent(input$delete_row, {
-    idx <- as.integer(
-      stringi::stri_extract_first_regex(
-        input$delete_row, "(?<=delete_row)\\d+$"
+    .germansentiment_rsess$interrupt()
+    .germansentiment_rsess$call(\(..doc_str){
+      if(!exists("...germansentiment_model")){
+        vns::use_vns_condaenv()
+        ..germansentiment_model <<- vns::load_germansentiment_model()
+      }
+      vns::calc_doc_germansentiment_tbl(
+        .doc_str=..doc_str, .germansentiment_model=..germansentiment_model
       )
-    )
-    vals$sen_vec <- vals$sen_vec[-idx]
+    }, args=list(..doc_str=dplyr::first(vals$sen_vec)))
   })
 
   observe({
-    updateTextInput(
-      session=session, inputId="sen_input-1",
-      value=vals$sen_vec[[1]]
+    invalidateLater(500)
+    .doc_germansentiment_tbl <- purrr::pluck(
+      .germansentiment_rsess$read(), "result"
     )
-    updateTextInput(
-      session=session, inputId="sen_input-2",
-      value=vals$sen_vec[[1]]
-    )
+    if(!is.null(.doc_germansentiment_tbl)){
+      .doc_germansentiment_tbl_rct(.doc_germansentiment_tbl)
+    }
   })
-
-  # output$parse_spacy_table <- gt::render_gt({
-  #
-  #   .table_data <- .doc_parse_spacy_tbl_rct() |> slice_min(doc_id, n=1)
-  #
-  #   if(nrow(.table_data) == 0){
-  #     NULL
-  #   }else{
-  #     .table_data |>
-  #       mutate(across(everything(), as.character)) |>
-  #       relocate(tok_str, .before=0) |>
-  #       tibble::rowid_to_column("rowid") |>
-  #       tibble::column_to_rownames("rowid") |>
-  #       as.matrix() |>
-  #       t() |>
-  #       tibble::as_tibble(rownames="feature") |>
-  #       gt::gt() |>
-  #       gt::tab_style(
-  #         style = list(
-  #           # gt::cell_fill(color = "#bababa"),
-  #           gt::cell_borders(color="#bababa"),
-  #           gt::cell_text(weight = "bold")
-  #         ),
-  #         locations = gt::cells_body(rows=1)
-  #       ) |>
-  #       gt::tab_options(column_labels.hidden=TRUE) # |>
-  #       # gt::cols_label(
-  #       #   doc_text_html = "Text",
-  #       #   doc_pol_norm = "sentidictwert"
-  #       # )
-  #
-  #   }
-  # })
-
-  # output$parse_text <- renderText({
-  #
-  #   .table_data <- .doc_parse_tbl_rct() |> slice_min(doc_id, n=1)
-  #
-  #   if(nrow(.table_data) == 0){
-  #     NULL
-  #   }else{
-  #     .table_data |>
-  #       pull(tok_str)
-  #       mutate(across(everything(), as.character)) |>
-  #       relocate(tok_str, .before=0) |>
-  #       tibble::rowid_to_column("rowid") |>
-  #       tibble::column_to_rownames("rowid") |>
-  #       as.matrix() |>
-  #       t() |>
-  #       tibble::as_tibble(rownames="feature") |>
-  #       gt::gt() |>
-  #       gt::tab_style(
-  #         style = list(
-  #           # gt::cell_fill(color = "#bababa"),
-  #           gt::cell_borders(color="#bababa"),
-  #           gt::cell_text(weight = "bold")
-  #         ),
-  #         locations = gt::cells_body(rows=1)
-  #       ) |>
-  #       gt::tab_options(column_labels.hidden=TRUE)
-  #   }
-  # })
 
   output$sentidict_table <- gt::render_gt({
 
@@ -363,8 +236,8 @@ function(input, output, session) {
   })
 
   output$germansentiment_score <- renderText({
-    if(.doc_germansentiment_tbl_task$status() == "success"){
-      .doc_germansentiment_tbl_task$result() |>
+    if(!is.null(.doc_germansentiment_tbl_rct())){
+      .doc_germansentiment_tbl_rct() |>
         slice_head(n=1) |>
         mutate(
           doc_class_lab =
@@ -423,15 +296,123 @@ function(input, output, session) {
 
   })
 
-  # timer <- reactive({
-  #   # invalidate 1 minute later
-  #   invalidateLater(1000 * 5)
-  #   shinyjs::runjs(paste0(
-  #     "$('[data-spy=\"scroll\"]').each(function () {",
-  #     "  var $spy = $(this).scrollspy('refresh')",
-  #     "})"
-  #   ))
-  #   message("adjusted scrollspy")
-  # })
-
 }
+
+# output$accordion <- renderUI({
+#
+#   .accordion_tbl <-
+#     .doc_sentidict_tbl_rct() |>
+#     dplyr::summarize(
+#       doc_text_html = stringi::stri_c(
+#         stringi::stri_c(
+#           "<span class='", tok_pol_lab, " bubble' data-toggle='tooltip' ",
+#           "data-placement='top' title='", tok_pol_num, "'>",
+#           tok_str_raw, "</span>"
+#         ),
+#         collapse=""
+#       ),
+#       doc_word_count = length(tok_str_raw),
+#       doc_pol_norm = sum(tok_pol_num) / doc_word_count,
+#       .by=doc_id
+#     ) |>
+#     mutate(
+#       doc_panel = list(
+#         accordion_panel(
+#           title=HTML(stringi::stri_c(
+#             "<table style='width:100%'><tr>",
+#             "  <td width=80%>", doc_text_html, "</td>",
+#             "  <td>",
+#             scales::label_number(
+#               accuracy=1e-3, drop0trailing=FALSE
+#             )(doc_pol_norm),
+#             "  </td>",
+#             "  <td>",
+#             as.character(shiny::actionButton(
+#               inputId = paste0("delete_row", doc_id),
+#               label=NULL,
+#               icon=icon("trash-can"),
+#               style="padding: 0; border: none; background: none;",
+#               onclick=stringi::stri_c(
+#                 "Shiny.setInputValue(",
+#                 "  'delete_row', this.id, {priority: 'event'}",
+#                 ")"
+#               )
+#             )),
+#             "  </td>",
+#             "<tr></table>"
+#           )),
+#           HTML(as.character(
+#             withMathJax("$$a^2 + b^2 = c^2$$")
+#           #   # stringi::stri_rand_lipsum(n_paragraphs=1),
+#           #   # style="font-size:1rem; --bs-body-font-size:1rem;"
+#           ))
+#         )
+#       ),
+#       .by=doc_id
+#     )
+#
+#   HTML(as.character(accordion(!!!.accordion_tbl$doc_panel, multiple=FALSE)))
+#
+# })
+
+# output$parse_spacy_table <- gt::render_gt({
+#
+#   .table_data <- .doc_parse_spacy_tbl_rct() |> slice_min(doc_id, n=1)
+#
+#   if(nrow(.table_data) == 0){
+#     NULL
+#   }else{
+#     .table_data |>
+#       mutate(across(everything(), as.character)) |>
+#       relocate(tok_str, .before=0) |>
+#       tibble::rowid_to_column("rowid") |>
+#       tibble::column_to_rownames("rowid") |>
+#       as.matrix() |>
+#       t() |>
+#       tibble::as_tibble(rownames="feature") |>
+#       gt::gt() |>
+#       gt::tab_style(
+#         style = list(
+#           # gt::cell_fill(color = "#bababa"),
+#           gt::cell_borders(color="#bababa"),
+#           gt::cell_text(weight = "bold")
+#         ),
+#         locations = gt::cells_body(rows=1)
+#       ) |>
+#       gt::tab_options(column_labels.hidden=TRUE) # |>
+#       # gt::cols_label(
+#       #   doc_text_html = "Text",
+#       #   doc_pol_norm = "sentidictwert"
+#       # )
+#
+#   }
+# })
+
+# output$parse_text <- renderText({
+#
+#   .table_data <- .doc_parse_tbl_rct() |> slice_min(doc_id, n=1)
+#
+#   if(nrow(.table_data) == 0){
+#     NULL
+#   }else{
+#     .table_data |>
+#       pull(tok_str)
+#       mutate(across(everything(), as.character)) |>
+#       relocate(tok_str, .before=0) |>
+#       tibble::rowid_to_column("rowid") |>
+#       tibble::column_to_rownames("rowid") |>
+#       as.matrix() |>
+#       t() |>
+#       tibble::as_tibble(rownames="feature") |>
+#       gt::gt() |>
+#       gt::tab_style(
+#         style = list(
+#           # gt::cell_fill(color = "#bababa"),
+#           gt::cell_borders(color="#bababa"),
+#           gt::cell_text(weight = "bold")
+#         ),
+#         locations = gt::cells_body(rows=1)
+#       ) |>
+#       gt::tab_options(column_labels.hidden=TRUE)
+#   }
+# })
